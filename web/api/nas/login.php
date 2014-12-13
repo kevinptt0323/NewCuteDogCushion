7db0cad5e156d23b7ecc64c584de990147015003
actkz <actkz.cs02@g2.nctu.edu.ts>
2014-12-13 16:24:28 +0800

    login func fixed

--- web/api/nas/login.php
+++ web/api/nas/login.php
@@ -1,9 +1,3 @@
-<!--form method="POST" action="http://ncdc.nctucs.net/adv,/cgi-bin/weblogin.cgi?"
- enctype="multipart/form-data">
-<!--input type="text" name="username" value=""/>
-<!--input type="password" name="password" value=""/>
-<!--input type="submit" value="login"> <INPUT type="reset">
-<!--/form>
 <?php
 function login($username,$password)
 {
@@ -11,13 +5,5 @@ function login($username,$password)
     $respense=curl_exec($url);
     echo $response;
     curl_close($url);
-    #$Rq= new HttpRequest("http://ncdc.nctucs.net/adv,/cgi-bin/weblogin.cgi?password=$password&username=$username",HttpRequest::METH_POST);
- #   try{
-  #      $Rq -> send();
-        
-   # }catch (HttpException $ex)
-   # {
-   #     echo $ex;
-   # }
 }
 ?>
