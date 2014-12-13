5ba62436b7ca438c2b762f33f24ff91147bfe479
Kevin Lin <kevinptt0323@gmail.com>
2014-12-14 00:30:34 +0800

    login.php: add interface

--- web/api/nas/login.php
+++ web/api/nas/login.php
@@ -1,9 +1,11 @@
 <?php
 function login($username,$password)
 {
-    $url=curl_init("http://ncdc.nctucs.net/adv,/cgi-bin/weblogin.cgi?password=$password&username=$username");
-    $respense=curl_exec($url);
-    echo $response;
-    curl_close($url);
+	$url=curl_init("http://ncdc.nctucs.net/adv,/cgi-bin/weblogin.cgi?password=$password&username=$username");
+	$response=curl_exec($url);
+	curl_close($url);
+}
+if( isset($_POST['username']) && isset($_POST['password']) ) {
+	login($_POST['username'], $_POST['password']);
 }
 ?>
