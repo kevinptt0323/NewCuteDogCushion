ef99638ce0fe379c8b0caabf2b6d44517f7ee124
Kevin Lin <kevinptt0323@gmail.com>
2014-12-13 17:49:54 +0800

    add fb-album

--- web/js/main.js
+++ web/js/main.js
@@ -1,6 +1,18 @@
 var fb = {
 	login : function(username, password) {
 		console.log("fb login as " + username + "/" + password);
+	},
+	albumInit : function() {
+		for(i=1; i<=50; ++i) {
+			$("#fb-album").append("<div class=\"photo thumbnail\">" + i + "</div>");
+		}
+	},
+	fadeIn : function(index) {
+		fadeIn = this.fadeIn;
+		console.log(index);
+		if( index==$(".photo.thumbnail").length ) return;
+		$(".photo.thumbnail").eq(index).fadeIn(500);
+		setTimeout(function(){fadeIn(index+1)}, 10);
 	}
 };
 var nas = {
@@ -9,6 +21,7 @@ var nas = {
 	}
 };
 $(function() {
+	fb.albumInit();
 	$("#fb-login .submit").on('click', function() {
 		fb.login($("#fb-username").val(), $("#fb-password").val());
 	});
