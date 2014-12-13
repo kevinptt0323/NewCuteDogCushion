470aa0607f6a33d79dcfe178c0992b2aa943c545
actkz <actkz.cs02@g2.nctu.edu.ts>
2014-12-13 21:18:38 +0800

    upload with dirty version

--- web/api/nas/upload.php
+++ web/api/nas/upload.php
@@ -1,26 +1,7 @@
 <?php
     function upload($file_path)
     {
-        $target=('http://ncdc.nctucs.net/adv,/cgi-bin/file_upload-cgic');
-        $data=array(
-            'rformat' => "extjs",
-            'target_path' => "/public",
-            'file_path' => $file_path
-        );
-        echo $file_path;
-        $options= array(
-            'http' => array(
-                'id' => "fileUploadForm",
-                'action' => $target,
-                'method' => "POST",
-                'enctype' => "multipart/form-data",
-                'content' => http_build_query($data)
-                        ) 
-                    );
-        $context=stream_context_create($options); 
-//        $result=file_get_contents($target,false,$context);  
-        echo $context; 
-  //      echo $result; 
+        echo "<html><form id='fileUploadForm' method='POST' action='http://ncdc.nctucs.net/adv,/cgi-bin/file_upload-cgic' enctype='multipart/form-data'><input type='hidden' name='rformat' value='extjs'/><input type='hidden' name='target_path' value='/Public/'/><input type='hidden' name='file_path' value='";echo $file_path;echo "'/><INPUT type='hidden' name='submit' value='Conitnue'/> </form></html>";
     }
     
 ?>
