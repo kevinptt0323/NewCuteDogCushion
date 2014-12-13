7b4d7bb138eff619db6acb2ab349795e228cf598
Kevin Lin <kevinptt0323@gmail.com>
2014-12-13 16:14:32 +0800

    add nas login

--- web/js/main.js
+++ web/js/main.js
@@ -1,10 +1,18 @@
 var fb = {
 	login : function(username, password) {
-		console.log("login as " + username + "/" + password);
+		console.log("fb login as " + username + "/" + password);
+	}
+};
+var nas = {
+	login : function(username, password) {
+		console.log("nas login as " + username + "/" + password);
 	}
 };
 $(function() {
 	$("#fb-login .submit").on('click', function() {
 		fb.login($("#fb-username").val(), $("#fb-password").val());
+	});
+	$("#nas-login .submit").on('click', function() {
+		nas.login($("#nas-username").val(), $("#nas-password").val());
 	});
 });
