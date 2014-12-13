e3dfa9fc60272e0ca2cfd70a006cc7ec8a52d059
actkz <actkz.cs02@g2.nctu.edu.ts>
2014-12-13 17:42:31 +0800

    upload v2

--- web/api/nas/upload.php
+++ web/api/nas/upload.php
@@ -5,18 +5,22 @@
         $data=array(
             'rformat' => "extjs",
             'target_path' => "/public",
-            'file_path' => "$file_path"
+            'file_path' => $file_path
         );
+        echo $file_path;
         $options= array(
             'http' => array(
                 'id' => "fileUploadForm",
+                'action' => $target,
                 'method' => "POST",
-                'enctype' => "multipart/form-data"0
-                'content' => http_build_query(data)
-                );
+                'enctype' => "multipart/form-data",
+                'content' => http_build_query($data)
+                        ) 
+                    );
         $context=stream_context_create($options); 
-        $result=file_get_contents($target,false,$context);   
-        echo $result; 
+//        $result=file_get_contents($target,false,$context);  
+        echo $context; 
+  //      echo $result; 
     }
     
 ?>
