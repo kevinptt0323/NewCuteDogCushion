2ad91bc3ec35fdc61ffcc29f1430bbb80cb1e588
actkz <brian558266@yahoo.com.tw>
2014-12-14 12:08:49 +0800

    fixed upload

--- web/api/nas/uploader.sh
+++ web/api/nas/uploader.sh
@@ -1,6 +1,6 @@
 #first argument is the target path, second argument is the file path
-if [ ! -f cookie ];then echo "no cookie";
-fi
+#if [ ! -f cookie ];then echo "no cookie";
+#fi
 echo $1
 echo $2
-curl -v -b cookie -c cookie -e ncdc.nctucs.net -include --form rformat=extjs --form target_path=$1 --form file_path=@$2 http://ncdc.nctucs.net/adv,/cgi-bin/file_upload-cgic
+result=`env /usr/local/bin/curl -v -b cookie -c cookie -e ncdc.nctucs.net -include --form rformat=extjs --form target_path=$1 --form file_path=@$2 http://ncdc.nctucs.net/adv,/cgi-bin/file_upload-cgic`
