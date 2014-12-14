29f1f0c349a7e00326168d6f28afd008714fcca5
Daniel Tsai <idaniel.twc@gmail.com>
2014-12-14 10:28:44 +0800

    bug fix in logout rm function

--- web/api/nas/logout.sh
+++ web/api/nas/logout.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
 curl -v -b cookie -c cookie -e ncdc.nctucs.net -d "perform=logout" http://ncdc.nctucs.net/adv,/cgi-bin/setuser.cgi
 if [ -f cookie ] ;then
     rm cookie
+elif [ -f lude ] ;then
     rm lude
 fi
