2d5674423d6fadd9d356cc538e2494db6c52da1b
Daniel Tsai <idaniel.twc@gmail.com>
2014-12-14 11:39:08 +0800

    add logout.php as hanlder

---
+++ web/api/nas/logout.php
@@ -0,0 +1,5 @@
+<?php
+if(isset($_GET['logout'])){
+    system("sh logout.sh");
+}
+?>
--- web/api/nas/logout.sh
+++ web/api/nas/logout.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -v -b cookie -c cookie -e ncdc.nctucs.net -d "perform=logout" http://ncdc.nctucs.net/adv,/cgi-bin/setuser.cgi
+curl -v -b cookie -c cookie -e ncdc.nctucs.net -d perform=logout http://ncdc.nctucs.net/adv,/cgi-bin/setuser.cgi
 if [ -f cookie ] ;then
     rm cookie
 elif [ -f lude ] ;then
