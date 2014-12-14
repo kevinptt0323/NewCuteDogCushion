daf4b9b1847faa29e9e575ae607ad676e5b6a402
actkz <brian558266@yahoo.com.tw>
2014-12-14 10:47:51 +0800

    fixed bug

--- web/api/nas/login.php
+++ web/api/nas/login.php
@@ -3,7 +3,7 @@
 function login($username,$passwd)
 {
     $cmd=escapeshellcmd("curl -b cookie -c cookie -e ncdc.nctucs.net http://ncdc.nctucs.net/adv,/cgi-bin/weblogin.cgi?username=$username&password=$passwd")
-    $run=shell_exec($cmd);
+    $run=shell_exec("$cmd");
     echo $run;
 }
 ?>
