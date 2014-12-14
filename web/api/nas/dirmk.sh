d64ffebabdd9296952342e5077d8cc079967dc62
actkz <brian558266@yahoo.com.tw>
2014-12-14 10:09:42 +0800

    add folder success

--- web/api/nas/dirmk.sh
+++ web/api/nas/dirmk.sh
@@ -1,4 +1,4 @@
 #first arg is the create path,second arg is the shared path
 if [ ! -f cookie ];then exit 1;
 fi
-curl -b cookie -c cookie -e http://ncdc.nctucs.net -include --form share=public --form path=/public/hahaha http://ncdc.nctucs.net/cmd,/ck6fup6/fileBrowser_main/create_folder
+curl -b cookie -c cookie -e http://ncdc.nctucs.net -include --form share=public --form path=/hahaha http://ncdc.nctucs.net/cmd,/ck6fup6/fileBrowser_main/create_folder
